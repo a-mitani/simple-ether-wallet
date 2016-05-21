5ea07c205bb2640e888676e1c22ce0c099f4a1a4
a-mitani <akiramitani1207@gmail.com>
2016-05-21 16:45:39 +0900

    added sendEther Template

--- client/lib/modules/init_session_vars.js
+++ client/lib/modules/init_session_vars.js
@@ -6,4 +6,14 @@ Session.setDefault('isMining', false);
 Session.setDefault('hashRate', 0);
 Session.setDefault('peerCount', 0);
 
+//送金関連の変数
+var initialFundInfo = {
+  amount:0,
+  fAddr:0x0,
+  tAddr:0x0,
+};
+Session.setDefault("sendEther.fundInfo", initialFundInfo);
+Session.setDefault("sendEther.estimatedGas", 0);
+Session.setDefault("sendEther.currentGasPrice", 0);
+
 };
